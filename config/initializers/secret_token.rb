1bb9e2d5516c00b682ea9d6ac7e3b788dc1a938b
hanchennz <chenhan22@gmail.com>
2014-02-17 21:07:21 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Upflow::Application.config.secret_key_base = '6289d72577c8094202e64bd74eb6f0fbf476bb11723bf04a298c80c6bafe2665bc5d6a76415908b94de8043c59c5eee6cff554a8c35fdfdbb107b8f3f68295da'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Upflow::Application.config.session_store :cookie_store, key: '_upflow_session'
