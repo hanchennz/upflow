e3fb40889f02a304d30703ee323153df64bb2b6f
hanchennz <chenhan22@gmail.com>
2014-02-19 23:53:53 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Upflow::Application.routes.draw do
-  devise_for :users
+  root to: 'home#index'
 
-  root 'home#index'
+  devise_for :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
